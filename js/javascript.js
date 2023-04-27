c04a7302e983cf72c151c2a1bb1ec7352bc3792a
Lucia <lucibarbero11@gmail.com>
2023-04-26 23:59:52 -0300

    Avance entrega 3

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,4 @@
-let nombreDeUsuario= prompt ("Ingresa su nombre");
-alert("Bienvenido/a" + " "+ nombreDeUsuario);
-
-const catalogoProductosVerano = [
+const catalogoProductos = [
     {
       idProducto:1,
       nombreProducto:"Remera DryFit",
@@ -16,13 +13,13 @@ const catalogoProductosVerano = [
     },
     {
       idProducto:3,
-      nombreProducto:"Short DryFit",
+      nombreProducto:"Short Webenit",
       precioProducto: 2200,
       stock: 12,
     },
     {
       idProducto:4,
-      nombreProducto:"medias",
+      nombreProducto:"Medias",
       precioProducto: 1500,
       stock: 25,
     },
@@ -32,73 +29,80 @@ const catalogoProductosVerano = [
       precioProducto: 5000,
       stock: 100,
     },
-]
-
-const catalogoProductosInvierno = [
   {
-    idProducto:1,
+    idProducto:6,
     nombreProducto:"Campera SoftShell",
     precioProducto: 5500,
     stock: 1,
   },
   {
-    idProducto:2,
+    idProducto:7,
     nombreProducto:"Pantalon Largo SoftShell",
     precioProducto: 4000,
     stock: 350,
   },
   {
-    idProducto:3,
+    idProducto:8,
     nombreProducto:"Buzo SoftShell",
     precioProducto: 5000,
     stock: 101,
   },
   {
-    idProducto:4,
+    idProducto:9,
     nombreProducto:"Campera SoftShell",
     precioProducto: 5000,
     stock: 105,
   },
   {
-    idProducto:5,
+    idProducto:10,
     nombreProducto:"Campera Rompeviento",
     precioProducto: 5800,
     stock: 410,
   },
   {
-    idProducto:6,
+    idProducto:11,
     nombreProducto:"Camperon largo",
     precioProducto: 9500,
     stock: 110,
   },
 ]
 
-let seccion = prompt("A que seccion desea ingresar?: \n *Seccion Verano \n *Seccion Invierno");
+function capturar() {
 
-function mostrarCatalogo(catalogo) {
-  console.log("Catalogo productos", seccion, ":");
-  for (let i = 0; i < catalogo.length; i++) {
-    alert(`${i + 1}. ${catalogo[i].nombreProducto} - $${catalogo[i].precioProducto}`);
-  }
-}
 
-switch (seccion) {
-    case "Verano":
-      mostrarCatalogo(catalogoProductosVerano);
-      const prodElegidoVer = prompt("¿Qué producto desea? Recuerde ingresar el nro tal cual ve en el catalogo");
-      const productoBuscadoVer = catalogoProductosVerano.find( pev => pev.idProducto == prodElegidoVer );
-      console.log(productoBuscadoVer);
-      break;
-    case "Invierno":
-      mostrarCatalogo(catalogoProductosInvierno);
-      const prodElegidoInv = prompt("¿Qué producto desea? Recuerde ingresar el nro tal cual ve en el catalogo");
-      const productoBuscadoInv = catalogoProductosVerano.find( pei => pei.idProducto == prodElegidoInv );
-      console.log(productoBuscadoInv);
-      break;
-    default:
-      alert("No ingresó un valor correcto");
-      break;
+    data= [
+      {
+        idProducto: document.getElementById("IdProducto").value,
+        cantidad: document.getElementById("amount").value
+      }
+    ]
+
+    const myString = JSON.stringify(data);
+  console.log(myString);
+  console.log(myString[1].idProducto);
+
+
 }
 
 
+
+
+/*function addRowProduct(){
+  var rowCount = $("#tableDetailBill tr").length;
+  //alert(rowCount);
+  $("#tableDetailBill").append('<tr>' + 
+  '<td>' + '<select id="IdProducto" name="IdProducto" style="width:650px" class="form-control" required>'+ 
+  '<option disabled selected>Producto</option> <option value="1">Remera Dry Fit</option> ' +
+  '<option value="2">Remera Cool Elastic </option>'+
+  '<option value="3">Short Webenit</option>'+
+  '<option value="4">Medias</option>'+
+  '<option value="5">Pollera Dama</option>'+
+  '<option value="6">Campera SoftShell</option>'+
+  '<option value="7">Pantalon Largo SoftShell</option>'+
+  '<option value="8">Buzo SoftShell</option>'+
+  '<option value="9">Campera SoftShell</option>'+
+  '<option value="10">Campera Rompeviento</option>'+
+  '<option value="11">Camperon largo</option>' + '</td>'+
+  '<td>' + '<input type="text" class="form-control" placeholder="cantidad" aria-label="cantidad" id="amount" name="amount"required>' +  '</td>');
+}*/
 
