d54faa7198ce756d50334791ad4d57bf650c5015
Lucia <lucibarbero11@gmail.com>
2023-05-18 23:08:00 -0300

    entrega

--- js/javascript.js
+++ js/javascript.js
@@ -1,105 +1,193 @@
-const catalogoProductos = [
-    {
-      idProducto:0,
-      nombreProducto:"Remera DryFit",
-      precioProducto: 3300,
-      stock: 10,
-    },
-    {
-      idProducto:1,
-      nombreProducto:"Remera Cool Elastic",
-      precioProducto: 3500,
-      stock: 11,
-    },
-    {
-      idProducto:2,
-      nombreProducto:"Short Webenit",
-      precioProducto: 2200,
-      stock: 12,
-    },
-    {
-      idProducto:3,
-      nombreProducto:"Medias",
-      precioProducto: 1500,
-      stock: 25,
-    },
-    {
-      idProducto:4,
-      nombreProducto:"Pollera Dama",
-      precioProducto: 5000,
-      stock: 100,
-    },
-  {
-    idProducto:5,
-    nombreProducto:"Campera SoftShell",
-    precioProducto: 5500,
-    stock: 1,
-  },
-  {
-    idProducto:6,
-    nombreProducto:"Pantalon Largo SoftShell",
-    precioProducto: 4000,
-    stock: 350,
-  },
-  {
-    idProducto:7,
-    nombreProducto:"Buzo SoftShell",
-    precioProducto: 5000,
-    stock: 101,
-  },
-  {
-    idProducto:8,
-    nombreProducto:"Campera SoftShell",
-    precioProducto: 5000,
-    stock: 105,
-  },
-  {
-    idProducto:9,
-    nombreProducto:"Campera Rompeviento",
-    precioProducto: 5800,
-    stock: 410,
-  },
-  {
-    idProducto:10,
-    nombreProducto:"Camperon largo",
-    precioProducto: 9500,
-    stock: 110,
-  },
-]
+document.addEventListener('DOMContentLoaded', () => {
 
-function capturar() {
+  const baseDeDatos = [
+    {
+      id:0,
+      nombre:"Remera DryFit",
+      precio: 3300,
+      imagen: 'aps2.jpeg'
+    },
+    {
+      id:1,
+      nombre:"Remera Cool Elastic",
+      precio: 3500,
+      imagen: 'karate.jpeg',
+    },
+    {
+      id:2,
+      nombre:"Short Webenit",
+      precio: 2200,
+      imagen: 'shortWeb.jpeg',
+    },
+    {
+      id:3,
+      nombre:"Medias",
+      precio: 1500,
+      imagen: 'karate.jpeg',
+    },
+    {
+      id:4,
+      nombre:"Musculosa Dama",
+      precio: 3300,
+      imagen: 'musculosa.jpeg',
+    },
+  {
+    id:5,
+    nombre:"Campera SoftShell",
+    precio: 5500,
+    imagen: 'camperaUnion.jpeg',
+  },
+  {
+    id:6,
+    nombre:"Pantalon Largo SoftShell",
+    precio: 4000,
+    imagen: 'pantalonLargo.jpeg',
+  },
+  {
+    id:7,
+    nombre:"Buzo SoftShell",
+    precio: 5000,
+    imagen: 'buzo.jpeg',
+  },
+  {
+    id:8,
+    nombre:"Campera Microfibra",
+    precio: 6000,
+    imagen: 'camperamuni.jpeg',
+  },
+  {
+    id:9,
+    nombre:"Campera Rompeviento",
+    precio: 5800,
+    imagen: 'camperaChero.jpeg',
+  },
+  {
+    id:10,
+    nombre:"Chomba Spum",
+    precio: 4500,
+    imagen: 'chomba.jpeg',
+  },
 
-    data= [
-      {
-        idProducto: document.getElementById("IdProducto").value,
-        cantidad: document.getElementById("amount").value
+  ];
+
+  let carrito = [];
+  const divisa = '$';
+  const DOMitems = document.querySelector('#items');
+  const DOMcarrito = 
+  document.querySelector('#carrito');
+  const DOMtotal = document.querySelector('#total');
+  const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+  const miLocalStorage = window.localStorage;
+  const DOMbotonFinalizar = document.querySelector('#boton-finalizar');
+
+  //Dibuja todos los productos*/
+
+  function renderizarProductos() {
+      baseDeDatos.forEach((info) => {
+          const miNodo = document.createElement('div');
+          miNodo.classList.add('card', 'col-sm-4');
+          const miNodoCardBody = document.createElement('div');
+          miNodoCardBody.classList.add('card-body');
+          const miNodoTitle = document.createElement('h5');
+          miNodoTitle.classList.add('card-title');
+          miNodoTitle.textContent = info.nombre;
+          const miNodoImagen = document.createElement('img');
+          miNodoImagen.classList.add('img-fluid');
+          miNodoImagen.setAttribute('src', info.imagen);
+          const miNodoPrecio = document.createElement('p');
+          miNodoPrecio.classList.add('card-text');
+          miNodoPrecio.textContent = `${divisa}${info.precio}`; 
+          const miNodoBoton = document.createElement('button');
+          miNodoBoton.classList.add('btn', 'btn-primary');
+          miNodoBoton.textContent = '+';
+          miNodoBoton.setAttribute('marcador', info.id);
+          miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+          miNodoCardBody.appendChild(miNodoImagen);
+          miNodoCardBody.appendChild(miNodoTitle);
+          miNodoCardBody.appendChild(miNodoPrecio);
+          miNodoCardBody.appendChild(miNodoBoton);
+          miNodo.appendChild(miNodoCardBody);
+          DOMitems.appendChild(miNodo);
+      });
+  }
+
+  function anyadirProductoAlCarrito(evento) {
+      carrito.push(evento.target.getAttribute('marcador'))
+      renderizarCarrito();
+      guardarCarritoEnLocalStorage();
+  }
+
+
+  function renderizarCarrito() {
+      DOMcarrito.textContent = '';
+      const carritoSinDuplicados = [...new Set(carrito)];
+      carritoSinDuplicados.forEach((item) => {
+          const miItem = baseDeDatos.filter((itemBaseDatos) => {
+              return itemBaseDatos.id === parseInt(item);
+          });
+          const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+              return itemId === item ? total += 1 : total;
+          }, 0);
+          const miNodo = document.createElement('li');
+          miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+          miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${divisa}${miItem[0].precio}`;
+          const miBoton = document.createElement('button');
+          miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+          miBoton.textContent = 'X';
+          miBoton.style.marginLeft = '1rem';
+          miBoton.dataset.item = item;
+          miBoton.addEventListener('click', borrarItemCarrito);
+          miNodo.appendChild(miBoton);
+          DOMcarrito.appendChild(miNodo);
+      });
+      DOMtotal.textContent = calcularTotal();
+  }
+
+  function borrarItemCarrito(evento) {
+      const id = evento.target.dataset.item;
+      carrito = carrito.filter((carritoId) => {
+          return carritoId !== id;
+      });
+      renderizarCarrito();
+      guardarCarritoEnLocalStorage();
+
+  }
+
+  function calcularTotal() {
+      return carrito.reduce((total, item) => {
+          const miItem = baseDeDatos.filter((itemBaseDatos) => {
+              return itemBaseDatos.id === parseInt(item);
+          });
+          return total + miItem[0].precio;
+      }, 0).toFixed(2);
+  }
+
+  function vaciarCarrito() {
+      carrito = [];
+      renderizarCarrito();
+      localStorage.clear();
+  }
+
+  function guardarCarritoEnLocalStorage () {
+      miLocalStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+
+  function cargarCarritoDeLocalStorage () {
+      if (miLocalStorage.getItem('carrito') !== null) {
+          carrito = JSON.parse(miLocalStorage.getItem('carrito'));
       }
-    ]
+  }
 
-  const Costo = catalogoProductos[data[0]["idProducto"]]["precioProducto"] * data[0]["cantidad"];
+  function finalizarComprar(){
+      alert("MUCHAS GRACIAS POR SU COMPRA");
+  }
 
-  console.log(Costo);
-  
-  document.getElementById('input').innerHTML = Costo ;
-}
+  // Eventos
+  DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+  DOMbotonFinalizar.addEventListener('click', finalizarComprar);
 
-
-/*function addRowProduct(){
-  var rowCount = $("#tableDetailBill tr").length;
-  //alert(rowCount);
-  $("#tableDetailBill").append('<tr>' + 
-  '<td>' + '<select id="IdProducto" name="IdProducto" style="width:650px" class="form-control" required>'+ 
-  '<option disabled selected>Producto</option> <option value="1">Remera Dry Fit</option> ' +
-  '<option value="2">Remera Cool Elastic </option>'+
-  '<option value="3">Short Webenit</option>'+
-  '<option value="4">Medias</option>'+
-  '<option value="5">Pollera Dama</option>'+
-  '<option value="6">Campera SoftShell</option>'+
-  '<option value="7">Pantalon Largo SoftShell</option>'+
-  '<option value="8">Buzo SoftShell</option>'+
-  '<option value="9">Campera SoftShell</option>'+
-  '<option value="10">Campera Rompeviento</option>'+
-  '<option value="11">Camperon largo</option>' + '</td>'+
-  '<td>' + '<input type="text" class="form-control" placeholder="cantidad" aria-label="cantidad" id="amount" name="amount"required>' +  '</td>');
-}*/
-
+  // Inicio
+  cargarCarritoDeLocalStorage();
+  renderizarProductos();
+  renderizarCarrito();
+});
